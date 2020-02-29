68fb6e119c7af17bced10223ef85f6d44405244a
Mukesh Kumar <mukesh.kumar@altudo.co>
2020-02-29 09:26:44 +0530

    Adding DependencyInjection to Solution

---
+++ src/Foundation/DependencyInjection/code/Infrastructure/MvcControllerServicesConfigurator.cs
@@ -0,0 +1,14 @@
+﻿namespace Hackathon.Boilerplate.Foundation.DependencyInjection.Infrastructure
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.DependencyInjection;
+    public class MvcControllerServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMvcControllers("Hackathon.Boilerplate.Feature.*");
+            serviceCollection.AddClassesWithServiceAttribute("Hackathon.Boilerplate.Feature.*");
+            serviceCollection.AddClassesWithServiceAttribute("Hackathon.Boilerplate.Foundation.*");
+        }
+    }
+}
---
+++ src/Foundation/DependencyInjection/code/ServiceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hackathon.Boilerplate.Foundation.DependencyInjection
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class ServiceAttribute : Attribute
+    {
+        
+        public ServiceAttribute()
+        {
+
+        }
+
+        public ServiceAttribute(Type serviceType)
+        {
+            this.ServiceType = serviceType;
+        }
+
+        public Lifetime Lifetime { get; set; } = Lifetime.Singleton;
+        public Type ServiceType { get; set; }
+    }
+}
